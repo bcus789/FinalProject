bbe04d85de4dbb90fa9a91027df21ee4cd6bca55
akaryrye <ralldrin@gmail.com>
2019-11-06 16:02:20 -0800

    some passport stuffs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+//const passport = require('./passport');
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -7,10 +8,16 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// ===== Passport ====
+//app.use(passport.initialize());
+//app.use(passport.session());
+
 // Add routes
 app.use(routes);
 
@@ -19,5 +26,5 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/testdb");
 
 // Start the server
 app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  console.log(`🌎 App listening on PORT: ${PORT}!`);
 });
