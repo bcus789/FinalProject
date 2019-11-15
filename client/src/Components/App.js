0f4329e7a29e4c6f2fb1a6b6644e92cd9306cc6a
bryanpdl <bryan@ui8.net>
2019-11-14 20:44:35 -0800

    landing page attempt

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,21 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import AgeVar from "./components/ageVar/ageVar.js";
-import "./components/ageVar/ageVar.css";
-import TopNav from "./components/TopNav/TopNav.js";
-import Jumbo from "./components/Jumbotron/Jumbo.js";
-import StreamBox from "./components/Carousel/Carousel.js";
-import "./base.css";
-// import Nav from "./components/SideNav/SideNav.js";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Carousel from "./Carousel/Carousel.js"
+import "./App.css";
+import SideNav from "./SideNav/SideNav.js"
+import Navbar from "./TopNav/TopNav.js";
 
 function App() {
   return (
     <div>
-      <TopNav />
-      <StreamBox />
-      <Router>
-        <div></div>
-      </Router>
+      <SideNav /> 
+      
+      <Carousel />
+      
+      <Navbar />
+      
+      
+              
     </div>
   );
 }
