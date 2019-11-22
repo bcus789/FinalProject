711eb175cc3cd80befc15a3565dc1a65b3671bf6
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-11-21 20:22:26 -0800

    added .env for db

--- server/routes/bets.js
+++ server/routes/bets.js
@@ -1,24 +1,23 @@
 const router = require("express").Router();
-const Bet = require('../db/models/bet');
+const Bet = require("../db/models/bet");
 
 // POST /api/bet/submit (submit new bet)
-router.route('/submit')
-  .post((req, res) => {
-    console.log(req.body);
-    const newBet = new Bet({
-      amount: req.body.amount,
-      event: 'event',
-      selection: 'selection'
-    })
-    newBet.save((err, bet) => {
-      if (err) return console.log(err);
-      res.json({
-        msg: "bet has successfully saved to the database",
-        bet: bet
-      });
-    })
-  })
+router.route("/submit").post((req, res) => {
+  console.log(req.body);
+  const newBet = new Bet({
+    amount: req.body.amount,
+    event: "event",
+    selection: "selection"
+  });
+  newBet.save((err, bet) => {
+    if (err) return console.log(err);
+    res.json({
+      msg: "bet has successfully saved to the database",
+      bet: bet
+    });
+  });
+});
 
-  //router.route('/')
+//router.route('/')
 
 module.exports = router;
