1d704702cbb3f79409a7799f3d105c3ceb4447cd
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-11-21 19:14:04 -0800

    added label

--- client/src/Components/Auth/login.js
+++ client/src/Components/Auth/login.js
@@ -1,10 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 //import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import axios from "axios";
 import "../Modal/Modal.css";
 
 class Login extends Component {
-
   state = {
     email: "",
     password: ""
@@ -13,34 +12,35 @@ class Login extends Component {
   // update state on form input
   handleChange = event => {
     const { name, value } = event.target;
-    this.setState({[name]: value });
+    this.setState({ [name]: value });
   };
 
   // post login info to /api/user/auth
-  handleSubmit = (e) => {
+  handleSubmit = e => {
     e.preventDefault();
-    console.log('logging in ' + this.state.email);
+    console.log("logging in " + this.state.email);
     let data = {
       email: this.state.email,
       password: this.state.password
-    }
+    };
 
-    axios.post("/api/user/auth", data)
+    axios
+      .post("/api/user/auth", data)
       .then(response => {
-        console.log(response)
+        console.log(response);
         if (response.status === 200) {
-          this.props.updateUser(response.data.email, true, response.data.token)
+          this.props.updateUser(response.data.email, true, response.data.token);
           window.localStorage.setItem({
             token: response.data.token,
             email: response.data.email
-          })
-          console.log('successfully logged in user: ' + this.state.email);
+          });
+          console.log("successfully logged in user: " + this.state.email);
         } else {
-          console.log('Log in failed');
+          console.log("Log in failed");
         }
       })
       .catch(err => console.log(err));
-  }
+  };
 
   render() {
     return (
@@ -48,36 +48,37 @@ class Login extends Component {
         <h2 className="login-txt">Login</h2>
         <form>
           <div>
-            
-            <label htmlFor="email">Email</label>
+            <label htmlFor="email">Email: </label>
             <input
               type="email"
               id="email"
               name="email"
               placeholder="Email"
               value={this.state.email}
-              onChange={this.handleChange} />
+              onChange={this.handleChange}
+            />
           </div>
 
           <div>
-          
-            <input className="password-input"
-                placeholder="password"
-                type="password"
-                name="password"
-                value={this.state.password}
-                onChange={this.handleChange} />
+            <label>Password: </label>
+            <input
+              className="password-input"
+              placeholder="password"
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
           </div>
 
           <div>
-            <button onClick={this.handleSubmit}
-                    type="submit">LOG IN
+            <button onClick={this.handleSubmit} type="submit">
+              LOG IN
             </button>
           </div>
-
         </form>
       </div>
-    )
+    );
   }
 }
 
