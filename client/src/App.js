71b362e99965232f61f9250bf56848358efaa842
unknown <h.e.ergaster@gmail.com>
2019-11-26 18:06:24 -0800

    about

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from './Pages/Home/Home'
-import About from './Pages/About/About'
-import Wallet from './Pages/Wallet/Wallet'
-import Events from './Pages/Events/Events'
-import axios from 'axios';
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Wallet from "./Pages/Wallet/Wallet";
+import Events from "./Pages/Events/Events";
+import axios from "axios";
 import LogModal from "./Components/Modal/Modal.js";
-// import AnteUp from "./Components/AnteUp/Dropdown";
 import "./App.css";
 
 class App extends Component {
-
   state = {
     userId: null,
     username: null,
@@ -22,21 +20,40 @@ class App extends Component {
   };
 
   componentDidMount() {
-    if (localStorage.getItem('token')) this.getUser();
+    if (localStorage.getItem("token")) this.getUser();
   }
 
-  updateUser = (userId, username, email, firstName, lastName, wallet, loggedIn) => {
-    this.setState({ userId, username, email, firstName, lastName, wallet, loggedIn });
-  }
+  updateUser = (
+    userId,
+    username,
+    email,
+    firstName,
+    lastName,
+    wallet,
+    loggedIn
+  ) => {
+    this.setState({
+      userId,
+      username,
+      email,
+      firstName,
+      lastName,
+      wallet,
+      loggedIn
+    });
+  };
 
   updateWallet = wallet => {
     this.setState(wallet);
-  }
+  };
 
   getUser = () => {
-    axios.get('/api/user/info', { headers: { "x-auth-token": localStorage.getItem('token') } })
+    axios
+      .get("/api/user/info", {
+        headers: { "x-auth-token": localStorage.getItem("token") }
+      })
       .then(response => {
-        console.log(response)
+        console.log(response);
         if (response) {
           this.updateUser(
             response.data._id,
@@ -45,10 +62,11 @@ class App extends Component {
             response.data.firstName,
             response.data.lastName,
             response.data.wallet,
-            true)
+            true
+          );
         } else {
-          console.log('No user or Invalid token');
-        };
+          console.log("No user or Invalid token");
+        }
       });
   };
 
@@ -56,24 +74,19 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <LogModal username={this.state.username}
+          <LogModal
+            username={this.state.username}
             loggedIn={this.state.loggedIn}
-            updateUser={this.updateUser} />
+            updateUser={this.updateUser}
+          />
           <Switch>
-           
-            <Route exact path='/' component={Home} />
-            <Route exact path='/events' component={Events} />
-            <Route exact path='/about' component={About} />
-            <Route exact path='/wallet' component={Wallet} />
-
+            <Route exact path="/" component={Home} />
+            <Route exact path="/events" component={Events} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/wallet" component={Wallet} />
           </Switch>
-          {/* <AnteUp userId={this.state.userId}
-                      username={this.state.username}
-                      wallet={this.state.wallet}
-                      loggedIn={this.state.loggedIn} /> */}
         </div>
       </Router>
-
     );
   }
 }
--- client/src/Pages/About/About.js
+++ client/src/Pages/About/About.js
@@ -1,17 +1,18 @@
 import React from "react";
-import SideNav from '../../Components/SideNav/SideNav.js';
-import Navbar from '../../Components/TopNav/TopNav.js';
-import '../../App.css'
+import SideNav from "../../Components/SideNav/SideNav.js";
+import Navbar from "../../Components/TopNav/TopNav.js";
+import Jumbo from "../../Components/Jumbotron/Jumbo.js";
+import "../../Components/Jumbotron/Jumbo.css";
+import "../../App.css";
 
 function About() {
-    return (
-
-        <div>
-            <SideNav />
-            <Navbar />
-            <h1>8=>~~</h1>
-        </div>
-    );
+  return (
+    <div>
+      <SideNav />
+      <Navbar />
+      <Jumbo />
+    </div>
+  );
 }
 
 export default About;
