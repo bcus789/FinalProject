45cda9d9967fecfb3e5b6eafc7e836bbdf349988
Victor Lopez <victorel94@gmail.com>
2019-11-26 19:03:51 -0800

    routing for close button

--- client/src/Components/Modal/Modal.js
+++ client/src/Components/Modal/Modal.js
@@ -1,56 +1,59 @@
 import React from "react";
 import Auth from "../Auth";
+import { Link } from 'react-router-dom'
 import Fade from 'react-reveal/Fade';
 import "./Modal.css";
 class LogModal extends React.Component {
-    state = { show: false }
-  
-    showModal = () => {
-      this.setState({ show: true });
-    }
-    
-    hideModal = () => {
-      this.setState({ show: false });
-    }
-      
-    render() {
-      return (
-        <main>
-          <Modal  show={this.state.show}
-                  handleClose={this.hideModal} >
-            <Auth username={this.props.username}
-                  loggedIn={this.props.loggedIn}
-                  updateUser={this.props.updateUser}
-                  hideModal={this.hideModal} />
-          </Modal>
-          <Fade right>
-            <button className="log-in"
-                    type='button'
-                    onClick={this.showModal}>LOG IN
-            </button>
-          </Fade>
-        </main>
-      )
-    }
+  state = { show: false }
+
+  showModal = () => {
+    this.setState({ show: true });
   }
-  
-  const Modal = ({ handleClose, show, children }) => {
-    const showHideClassName = show ? 'modal display-block' : 'modal display-none';
-  
+
+  hideModal = () => {
+    this.setState({ show: false });
+  }
+
+  render() {
     return (
-      <div className={showHideClassName}>
-        <Fade up>
+      <main>
+        <Modal show={this.state.show}
+          handleClose={this.hideModal} >
+          <Auth username={this.props.username}
+            loggedIn={this.props.loggedIn}
+            updateUser={this.props.updateUser}
+            hideModal={this.hideModal} />
+        </Modal>
+        <Fade right>
+          <button className="log-in"
+            type='button'
+            onClick={this.showModal}>LOG IN
+            </button>
+        </Fade>
+      </main>
+    )
+  }
+}
+
+const Modal = ({ handleClose, show, children }) => {
+  const showHideClassName = show ? 'modal display-block' : 'modal display-none';
+
+  return (
+    <div className={showHideClassName}>
+      <Fade up>
         <section className='modal-main'>
           {children}
-          <button className="close-bttn"
-            onClick={handleClose}
-          >
-            CLOSE
+          <Link to='/'>
+            <button className="close-bttn"
+              onClick={handleClose}
+            >
+              CLOSE
           </button>
+          </Link>
         </section>
-        </Fade>
-      </div>
-    );
-  };
+      </Fade>
+    </div>
+  );
+};
 
-  export default LogModal;
+export default LogModal;
